2a1114bb93af59dc719680e7b17d86af98309258
sertmer <scottertmer@gmail.com>
2020-01-29 07:24:58 -0700

    Configure knex

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
