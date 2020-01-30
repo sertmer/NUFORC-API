b156c516e849448509b39073d1dc0daa1ad10841
sertmer <scottertmer@gmail.com>
2020-01-30 11:03:24 -0700

    Add get for all locations

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get('/', (request, response) => {
   response.send('Welcome to NUFORC');
 });
 
+app.get('/api/v1/locations', async (req, res) => {
+  try {
+    const locations = await database('locations').select();
+    res.status(200).json(locations)
+  } catch(error) {
+    res.status(500).json({ error })
+  }
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
