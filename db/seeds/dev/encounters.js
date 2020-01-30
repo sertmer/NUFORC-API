1528a738431881c474ba8a8ea0e1ecc0e31dd7f5
sertmer <scottertmer@gmail.com>
2020-01-30 09:40:49 -0700

    Complete Seeding

--- db/seeds/dev/encounters.js
+++ db/seeds/dev/encounters.js
@@ -1,25 +1,42 @@
+const encountersData = require('../../../data/encountersData')
+
+const createLocation = async (knex, location) => {
+  const locationId = await knex('locations').insert({
+    city: location.city,
+    state: location.state
+  }, 'id');
+
+  let encounterPromises = location.encounters.map(encounter => {
+    const {description, shape, duration, report_link, date_time, date_posted} = encounter
+    return createEncounter(knex, {
+      description,
+      shape, 
+      duration,
+      report_link,
+      date_time,
+      date_posted,
+      location_id: locationId[0]
+    })
+  });
+  
+  return Promise.all(encounterPromises)
+};
+
+const createEncounter = (knex, encounter) => {
+  return knex('encounters').insert(encounter)
+};
 
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
   try {
     await knex('encounters').del()
     await knex('locations').del()
 
-    const locationId = await knex('locations').insert({
-      city: 'Denver',
-      state: 'CO'
-    }, 'id')
-    return knex('encounters').insert([
-      {
-        description: 'saw some weird lights',
-        shape: 'lights',
-        duration: '10 minutes',
-        report_link: 'url.com',
-        date_time: '12/12/12 12:00am',
-        date_posted: 'today',
-        location_id: locationId[0]
-      }
-    ])
+    let locationPromises = encountersData.map(location => {
+      return createLocation(knex, location)
+    })
+
+    return Promise.all(locationPromises)
+
   } catch (error) {
     console.log(`error seeding data: ${error}`)
   }
