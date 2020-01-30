c5123d31c62471a5c9eef9ad26ff7bb026829e46
sertmer <scottertmer@gmail.com>
2020-01-30 12:43:59 -0700

    Add all get endpoints

--- server.js
+++ server.js
@@ -20,6 +20,46 @@ app.get('/api/v1/locations', async (req, res) => {
   }
 })
 
+app.get('/api/v1/locations/:id', async (req, res) => {
+  try {
+    const location = await database('locations').where('id', req.params.id).select();
+    if (location.length) {
+      res.status(200).json(location)
+    } else {
+      res.status(404).json({
+        error: `Could not find locations with id of ${req.params.id}.`
+      })
+    }
+  } catch(error) {
+    res.status(500).json({ error })
+  }
+})
+
+app.get('/api/v1/encounters', async (req, res) => {
+  try {
+    const encounters = await database('encounters').select();
+    res.status(200).json(encounters)
+  } catch(error) {
+    res.status(500).json({ error })
+  }
+})
+
+app.get('/api/v1/encounters/:id', async (req, res) => {
+  try {
+    const encounter = await database('encounters').where('id', req.params.id).select();
+    if (encounter.length) {
+      res.status(200).json(encounter)
+    } else {
+      res.status(404).json({
+        error: `Could not find encounters with id of ${req.params.id}.`
+      })
+    }
+  } catch(error) {
+    res.status(500).json({ error })
+  }
+})
+
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
 });
